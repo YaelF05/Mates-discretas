6e70987bb220c9b6c80484a8287e2d930fafa389
Yael Franco Toledo <143737848+YaelF05@users.noreply.github.com>
2024-03-11 14:58:19 -0600

    Update sucesion6.py

--- sucesiones/sucesion6.py
+++ sucesiones/sucesion6.py
@@ -7,6 +7,5 @@ Created on Thu Feb 29 19:01:47 2024
 numero = int(input("Ingresa un número: "))
 
 for i in range(1, numero + 1):
-    numerador = (-1)**(i-1) * (i - 1)
     print((-1)**(i-1) * (i - 1), "/", i)
 
